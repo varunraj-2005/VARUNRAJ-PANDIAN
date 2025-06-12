225d09afb4d05c63685efd27dd20e0df1ffc1dd6
varunraj-2005 <rajapandian1412@gmail.com>
2025-06-12 18:22:12 +0530

    Create strongpasswordgenerator.py

---
+++ securecrypt/strongpasswordgenerator.py
@@ -0,0 +1,5 @@
+import random, string
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
